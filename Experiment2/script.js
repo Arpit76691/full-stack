fde342814d0bc1eeede8cb99e1db7336ef2dd83e
Arpit76691 <ay8435290@gmail.com>
2026-02-03 10:41:31 +0530

    Delete Experiment2 directory

--- Experiment2/script.js
+++
@@ -1,7 +0,0 @@
-
-const textInput = document.getElementById("textInput");
-const characterCounter = document.getElementById("counter");
-
-textInput.addEventListener("input", () => {
-    characterCounter.textContent = textInput.value.length;
-});
